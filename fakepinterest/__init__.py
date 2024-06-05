197ede3f55070e687942288d7abb69ba265cb21a
Thiago-Scofield <thiprofin@yahoo.com.br>
2024-06-05 19:17:00 -0300

    primeiro commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,19 @@
+# onde criaremos nosso site
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = '3079a6427f139b5f9415f680e81c890b'
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
