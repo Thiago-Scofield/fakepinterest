7bb2449576e005030c3ffe4a52b5c791377d02a1
Thiago-Scofield <thiprofin@yahoo.com.br>
2024-06-06 20:46:57 -0300

    Nova Key

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SECRET_KEY'] = '3079a6427f139b5f9415f680e81c890b'
+app.config['SECRET_KEY'] = 'KEY'
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
